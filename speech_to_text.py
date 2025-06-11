53c85d2531fed22135073cb46a548aa0f20e9e1f
LMcIntosh99 <lsmcintosh99@gmail.com>
2025-06-11 21:03:25 +0100

    Speech to text python

---
+++ speech_to_text.py
@@ -0,0 +1,48 @@
+import speech_recognition as sr
+import pyttsx3
+import tkinter as tk
+from threading import Thread
+
+class SpeechRecognitionGUI:
+    def __init__(self, master):
+        self.master = master
+        self.master.title("Speech Recognition")
+
+        self.status_label = tk.Label(master, text="Waiting for prompt...")
+        self.status_label.pack()
+
+        self.recognizer = sr.Recognizer()
+        self.engine = pyttsx3.init()
+
+        self.listen_button = tk.Button(master, text="Start Listening", command=self.start_listening)
+        self.listen_button.pack()
+
+    def start_listening(self):
+        self.listen_button.config(state="disabled")
+        self.status_label.config(text="Adjusting for ambient noise...")
+        self.listen_thread = Thread(target=self.listen)
+        self.listen_thread.start()
+
+    def listen(self):
+        with sr.Microphone() as source:
+            self.recognizer.adjust_for_ambient_noise(source, duration=1)
+
+            self.status_label.config(text="Listening...")
+            audio = self.recognizer.listen(source, phrase_time_limit=10)
+
+            self.status_label.config(text="Processing...")
+            try:
+                text = self.recognizer.recognize_google(audio)
+                self.status_label.config(text="Heard: " + text)
+                #response = ask_gpt(text)
+                print(text)
+            except sr.UnknownValueError:
+                self.status_label.config(text="Unable to recognize speech")
+            except sr.RequestError:
+                self.status_label.config(text="Error requesting results")
+            finally:
+                self.listen_button.config(state="normal")
+
+root = tk.Tk()
+my_gui = SpeechRecognitionGUI(root)
+root.mainloop()
